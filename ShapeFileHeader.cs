ee5cfb4ebb56a7194eee3feebdbe460a4b9e0de5
halmaia <halmaia@gmail.com>
2020-06-09 19:26:46 +0200

    Update ShapeFileHeader.cs

--- ShapeFileHeader.cs
+++ ShapeFileHeader.cs
@@ -38,82 +38,73 @@ namespace ShpRead
         double Z { get; set; }
     }
 
-    [StructLayout(LayoutKind.Explicit, Size = 100)]
-    [DebuggerDisplay("{_ShapeType} (Xmin: {Xmin}; Ymin: {Ymin}; Xmax: {Xmax}; Ymax: {Ymax})")]
+    [DebuggerDisplay("{ShapeType} (Xmin: {Xmin}; Ymin: {Ymin}; Xmax: {Xmax}; Ymax: {Ymax})")]
+    [StructLayout(LayoutKind.Sequential)]
     public struct ShapeFileHeader
     {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static int Swap(int value)
-        {
-            //Original
-            //uint v = (uint)value;
-            //v = (v >> 16) | (v << 16);
-            //return (int)(((v & 0xFF00FF00) >> 8) | ((v & 0x00FF00FF) << 8));
-
-            //Optimized
-            uint v = (uint)((int)((uint)value >> 16) | value << 16);
-            return (int)((uint)((int)v & -16711936) >> 8 | (v & 0xFF00FF) << 8);
-        }
-
         [System.Security.SecuritySafeCritical]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private unsafe static double NaNToMin(double value)
+        private static unsafe int Reverse(int value)
         {
-            if ((*(ulong*)(&value) & 0x7FFFFFFFFFFFFFFF) <= 9218868437227405312uL)
-                return value;
-            return double.MinValue;
+            byte* p = (byte*)&value;
+            return (*p << 24) | (p[1] << 16) | (p[2] << 8) | p[3];
+        }
+
+        private const int DefaultFileCode = 9994;
+        private readonly int _FileCode;
+        [DefaultValue(DefaultFileCode)] public int FileCode { get => Reverse(_FileCode); }
+
+        private const int DefaultUnusedValue = 0;
+        private readonly int _Unused1, _Unused2, _Unused3, _Unused4, _Unused5;
+        [DefaultValue(DefaultUnusedValue)] public int Unused1 { get => Reverse(_Unused1); }
+        [DefaultValue(DefaultUnusedValue)] public int Unused2 { get => Reverse(_Unused2); }
+        [DefaultValue(DefaultUnusedValue)] public int Unused3 { get => Reverse(_Unused3); }
+        [DefaultValue(DefaultUnusedValue)] public int Unused4 { get => Reverse(_Unused4); }
+        [DefaultValue(DefaultUnusedValue)] public int Unused5 { get => Reverse(_Unused5); }
+
+        private int _FileLength;
+        [DefaultValue(0)] public int FileLenght { get => Reverse(_FileLength); set => _FileLength = Reverse(value); }
+
+        private const int DefaultFileVersion = 1000;
+        [DefaultValue(DefaultFileVersion)] public int Version { get; }
+
+        [DefaultValue(ShapeType.NullShape)] public ShapeType ShapeType { get; }
+
+        [DefaultValue(0.0)] public double Xmin { get; set; }
+        [DefaultValue(0.0)] public double Ymin { get; set; }
+        [DefaultValue(0.0)] public double Xmax { get; set; }
+        [DefaultValue(0.0)] public double Ymax { get; set; }
+        [DefaultValue(0.0)] public double Zmin { get; set; }
+        [DefaultValue(0.0)] public double Zmax { get; set; }
+        [DefaultValue(0.0)] public double Mmin { get; set; }
+        [DefaultValue(0.0)] public double Mmax { get; set; }
+
+        public ShapeFileHeader(int fileLenght, ShapeType shapeType, double xmin, double ymin, double xmax, double ymax, double zmin, double zmax, double mmin, double mmax) : this()
+        {
+            _FileCode = Reverse(DefaultFileCode);
+
+            _Unused1 = DefaultUnusedValue;
+            _Unused2 = DefaultUnusedValue;
+            _Unused3 = DefaultUnusedValue;
+            _Unused4 = DefaultUnusedValue;
+            _Unused5 = DefaultUnusedValue;
+
+            Version = DefaultFileVersion;
+
+
+            FileLenght = fileLenght;
+            ShapeType = shapeType;
+            Xmin = xmin;
+            Ymin = ymin;
+            Xmax = xmax;
+            Ymax = ymax;
+            Zmin = zmin;
+            Zmax = zmax;
+            Mmin = mmin;
+            Mmax = mmax;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static double MinToNaN(double value)
-        {
-            if (value == double.MinValue)
-                return double.NaN;
-            return value;
-        }
-
-        /* Big endian fields */
-        [FieldOffset(0)] private int _FileCode;
-        [FieldOffset(4)] private int _Unused0;
-        [FieldOffset(8)] private int _Unused1;
-        [FieldOffset(12)] private int _Unused2;
-        [FieldOffset(16)] private int _Unused3;
-        [FieldOffset(20)] private int _Unused4;
-        [FieldOffset(24)] private int _FileLength;
-        [FieldOffset(28)] private int _Version;
-
-        /* Little endian fields */
-        [FieldOffset(32)] private ShapeType _ShapeType;
-        [FieldOffset(36)] private double _Xmin;
-        [FieldOffset(44)] private double _Ymin;
-        [FieldOffset(52)] private double _Xmax;
-        [FieldOffset(60)] private double _Ymax;
-        [FieldOffset(68)] private double _Zmin;
-        [FieldOffset(76)] private double _Zmax;
-        [FieldOffset(84)] private double _Mmin;
-        [FieldOffset(92)] private double _Mmax;
-
-        /* Big endian properties */
-        public int FileCode { get => Swap(_FileCode); set => _FileCode = Swap(value); }
-        public int Unused0 { get => Swap(_Unused0); set => _Unused0 = Swap(value); }
-        public int Unused1 { get => Swap(_Unused1); set => _Unused1 = Swap(value); }
-        public int Unused2 { get => Swap(_Unused2); set => _Unused2 = Swap(value); }
-        public int Unused3 { get => Swap(_Unused3); set => _Unused3 = Swap(value); }
-        public int Unused4 { get => Swap(_Unused4); set => _Unused4 = Swap(value); }
-        public int FileLength { get => Swap(_FileLength); set => _FileLength = Swap(value); }
-
-        /* Little endian properties */
-        public int Version { get => _Version; set => _Version = value; }
-        public ShapeType ShapeType { get => _ShapeType; set => _ShapeType = value; }
-        public double Xmin { get => _Xmin; set => _Xmin = value; }
-        public double Ymin { get => _Ymin; set => _Ymin = value; }
-        public double Xmax { get => _Xmax; set => _Xmax = value; }
-        public double Ymax { get => _Ymax; set => _Ymax = value; }
-        public double Zmin { get => MinToNaN(_Zmin); set => _Zmin = NaNToMin(value); }
-        public double Zmax { get => MinToNaN(_Zmax); set => _Zmax = NaNToMin(value); }
-        public double Mmin { get => MinToNaN(_Mmin); set => _Mmin = NaNToMin(value); }
-        public double Mmax { get => MinToNaN(_Mmax); set => _Mmax = NaNToMin(value); }
-
         [System.Security.SecuritySafeCritical]
         public unsafe ShapeFileHeader(in byte[] buffer)
         {
